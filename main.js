410cae9171e3fdd6673ac87e3bdf940dbe978119
farukwebcode21 <farukahmadmk@yahoo.com>
2023-07-07 21:00:45 +0600

    almost done but menu is some issue

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".menu");
 const menuBtn = document.querySelector(".menu-btn");
-const checkpoints = document.querySelector(".scroll-checkpoint");
-const links = document.querySelector(".menu ul a");
-menuBtn.addEventListener("click", ()=>{
-    menu.classList.toggle("menu-open")
+const checkpoints = document.querySelectorAll(".scroll-checkpoint");
+const links = document.querySelectorAll(".menu ul a");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("menu-open");
 });
 
-window.addEventListener("scroll", ()=>{
-    if(pageYOffset > 50){
-        navbar.classList.add("navbar-active");
-    }else{
-        navbar.classList.remove("navbar-active");
+window.addEventListener("scroll", () => {
+  if (pageYOffset > 50) {
+    navbar.classList.add("navbar-active");
+  } else {
+    navbar.classList.remove("navbar-active");
+  }
+  for (let i = 0; i < checkpoints.length; i++) {
+    if (pageYOffset > checkpoints[i].offsetTop - 50) {
+      links.forEach((link) => link.classList.remove("active"));
+      links[i].classList.add("active");
     }
-    for(let i =0; i<checkpoints.length; i++){
-        if(pageYOffset > checkpoints[i].offsetTop - 50){
-            links.forEach(link =>link.classList.remove("active"));
-            links[i].classList.add("active");
-        }
-    }
+  }
 });
